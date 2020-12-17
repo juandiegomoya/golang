c1ee5be49c0f9b8bac1de1c9192e62ab5b992633
Juan Moya <juan.moya@grupor5.com>
2020-12-16 23:18:04 -0500

    04 datos de teclado

---
+++ 04_datos_consola/main.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+var num1 int
+var num2 int
+
+func main() {
+	//para pedir datos de consola se realiza con scanf
+	//para windows Scanln
+	fmt.Println("Ingrese el numero 1")
+	fmt.Scanf("%d", &num1)
+
+	fmt.Println("Ingrese el numero 2")
+	fmt.Scanf("%d", &num2)
+}
